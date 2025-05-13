c178e436be7ddd9039e931b84600172c81e05608
Gmi91 <gmi91.dev@gmail.com>
2025-05-13 17:24:00 +0800

    Initialization
    Upload files

---
+++ GMNotification/NotificationObserver.cs
@@ -0,0 +1,73 @@
+﻿using Accessibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMNotification
+{
+    internal class NotificationObserver
+    {
+        internal readonly object observer;
+        internal List<Notification> RegisteredNotifications => _RegisteredNotifications;
+        private List<Notification> _RegisteredNotifications { get; set; }
+
+        internal NotificationObserver(object observer)
+        {
+            this.observer = observer;
+            this._RegisteredNotifications = new List<Notification>();
+        }
+
+        internal void AddNotification(Notification notification)
+        {
+            Notification? noti = _RegisteredNotifications.Where(e => e.Name == notification.Name).ToList().FirstOrDefault();
+
+            // 注册通知时，最后一次会覆盖之前的
+            if (noti != null)
+            {
+                RemoveNotification(noti);
+            }
+            _RegisteredNotifications.Add(notification);
+        }
+
+        internal void RemoveNotification(string name)
+        {
+            Notification? noti = _RegisteredNotifications.Where(x => x.Name == name).FirstOrDefault();
+
+            if (noti != null)
+            {
+                RemoveNotification(noti);
+            }
+        }
+
+        internal void RemoveNotification(Notification notification)
+        {
+            _RegisteredNotifications.Remove(notification);
+        }
+
+        internal bool ContainsNotification(string name)
+        {
+            Notification? noti = _RegisteredNotifications.Where(s => s.Name == name).ToList().FirstOrDefault();
+            if (noti != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        internal void Post(string name, object? userInfo = null)
+        {
+            Notification? noti = _RegisteredNotifications.Where(s => s.Name == name).ToList().FirstOrDefault();
+            if (noti != null)
+            {
+                Notification newNoti = new Notification(name);
+                newNoti.SetUserInfo(userInfo);
+                // 传一个新的实例，防止操作污染
+                noti.CallbackAction(newNoti);
+            }
+        }
+
+    }
+}
---
+++ NotificationSample/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GMNotification;
+
+namespace NotificationSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NotificationCenter.DefaultCenter().AddObserver(this, "resuest.start", noti =>
+            {
+                Update("获取数据中...");
+                
+            });
+
+            NotificationCenter.DefaultCenter().AddObserver(this, "resuest.success", ResuestSuccess);
+
+            NotificationCenter.DefaultCenter().AddObserver(this, "resuest2.success", ResuestSuccess);
+
+            var manager = new HTTPManager();
+
+            NotificationCenter.DefaultCenter().RemoveObserver(this);
+
+            manager.Request();
+        }
+
+        private void ResuestSuccess(Notification noti)
+        {
+            //NotificationCenter.DefaultCenter().RemoveObserver(this, "resuest2.success");
+
+            Dictionary<string, string> dic = noti.UserInfo as Dictionary<string, string>;
+
+            if (dic != null)
+            {
+                var t = $"{noti.Name}: {dic["name"]}， {dic["age"]}";
+                Update(t);
+            }
+            
+        }
+
+        private void Update(string text)
+        {
+            Application.Current.Dispatcher.Invoke((Action)delegate ()
+            {
+                ContentText.Text = text;
+            });
+        }
+
+    }
+
+    class HTTPManager()
+    {
+        public void Request()
+        {
+            NotificationCenter.DefaultCenter().Post("resuest.start");
+
+            Task task = new Task(GetUserInfo);
+            task.Start();
+
+            Task task2 = new Task(GetUserInfo2);
+            task2.Start();
+        } 
+
+        private void GetUserInfo()
+        {
+
+            Thread.Sleep(5000);
+
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("name", "张三");
+            dic.Add("age", "28");
+
+            NotificationCenter.DefaultCenter().Post("resuest.success", dic);
+
+        }
+
+        private void GetUserInfo2()
+        {
+
+            Thread.Sleep(15000);
+
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("name", "李四");
+            dic.Add("age", "20");
+
+            NotificationCenter.DefaultCenter().Post("resuest2.success", dic);
+        }
+
+    }
+
+}
